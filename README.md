24741354e8343dc5247a2cc2cb2bc27189b89399
flodardev <63950579+flodardev@users.noreply.github.com>
2020-07-04 02:09:14 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Notes App
+
+Note taking app built with React
+
+## Deployed
+
+https://radolfdailyjournal.herokuapp.com/
+
+## Built With
+
+* [React.js](https://reactjs.org/) - A JavaScript library for building user interfaces
+
+## Authors
+
+* **Radolf Lasa** - *Initial work* - [flodardev](https://github.com/flodardev)
+
+
+## Acknowledgments
+
+* Part of The Complete 2020 Web Development Bootcamp by London App Brewery
---
+++ build/precache-manifest.3027b53616474a8b983927d250704ac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8698ee2d840a0d61f0c4fcffec4facfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa5555772f4267fd17d",
+    "url": "/static/js/2.c18bcafc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c18bcafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517253cf12ed42823295",
+    "url": "/static/js/main.6bb7af96.chunk.js"
+  },
+  {
+    "revision": "b4d12b70b7d90d24ec54",
+    "url": "/static/js/runtime-main.9496a8dc.js"
+  }
+]);
